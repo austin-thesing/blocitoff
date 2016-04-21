c3423ed5ce1186c920adf864118ac2e08c819290
Austin <admin@designxdevelop.com>
2016-04-20 17:35:12 -0700

    Devise Configuration user story completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  root to: 'welcome#index' # sets the root of the website/application
+
+
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

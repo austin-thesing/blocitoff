9bc4a0c7d29ffa0690849e5ba339cdb7246cadf5
Austin <admin@designxdevelop.com>
2016-04-25 17:01:01 -0700

    Added User Profiles & Root Redirect Method
    Added Users#Show view for displaying user profiles
    
    Created #root_redirect in Users_Controller to redirect the user logging in to their profile.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def show
-    
+    @user = User.find(params[:id])
+  end
+
+  def root_redirect # => redirects a user upon login to their Profile/Task List /users/:id
+    redirect_to user_path(current_user)
   end
 end

b7c7c7c7e6e5aaa9559386908336398343af0934
Austin <admin@designxdevelop.com>
2016-04-27 11:01:25 -0700

    CP5 Completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find(params[:id])
+    @items = @user.items
+    @item = Item.new
   end
 
   def root_redirect # => redirects a user upon login to their Profile/Task List /users/:id
     redirect_to user_path(current_user)
   end
+
 end
